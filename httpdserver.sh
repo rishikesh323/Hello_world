e9446d814cea89a01d86f27532524fae97f62150
rishikesh323 <80004288+rishikesh323@users.noreply.github.com>
2024-12-03 09:53:50 +0530

    Update httpdserver.sh

--- httpdserver.sh
+++ httpdserver.sh
@@ -1,3 +1,6 @@
 echo "HELLO WORLD" >>  /var/www/html/index.html 
+systemctl enable httpd
+systemctl start httpd
 firewall-cmd --add-service=http --permanent 
 firewall-cmd --reload-all 
+curl localhost:80
